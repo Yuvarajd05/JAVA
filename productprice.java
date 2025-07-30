2aec39d759bf77f4a6410a8190e0898d34bfd952
Yuvaraj D <126308145+Yuvarajd05@users.noreply.github.com>
2025-07-30 12:46:53 +0530

    Add files via upload

---
+++ productprice.java
@@ -0,0 +1,12 @@
+public class productprice {
+    public static void main(String[]args){
+        int amazon= 14999;
+        int flipkart= 13499;
+
+        if(amazon>flipkart){
+            System.out.println("flipkart is cheap");
+        }else{
+            System.err.println("Amazon is cheaper");
+        }
+        }
+    }
