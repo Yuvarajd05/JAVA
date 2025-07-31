c8991ae2f3d435d0c61a7e1790dc1b569fe4d48c
Yuvaraj D <126308145+Yuvarajd05@users.noreply.github.com>
2025-07-31 21:30:34 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,11 @@
+public class num {
+    public static void main(String[] args) {
+        int i=0;
+        do { 
+            i=i+1;
+            System.out.println(i);
+        } while (i<11);
+       
+    }
+    
+}
---
+++ numberss.java
@@ -0,0 +1,9 @@
+public class numberss {
+    public static void main(String[] args) {
+        int i =0;
+        while(i<11){
+            i=i+1;
+            System.out.println(i);
+        }
+    }
+}
