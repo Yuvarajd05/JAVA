108d514a3948219ff0d526bb5df2c318e7b41a67
Yuvaraj D <126308145+Yuvarajd05@users.noreply.github.com>
2025-08-13 00:05:57 +0530

    Add files via upload

---
+++ charat.java
@@ -0,0 +1,12 @@
+public class charat {
+    public static void main(String[] args) {
+        String firstname="tony";
+        String lastname ="stark";
+        String fullname =firstname+lastname;
+        System.out.println(fullname.length());
+    
+    for(int i=0;i<fullname.length();i++){
+        System.out.println(fullname.charAt(i));
+    }
+}
+}
---
+++ stringscompare.java
@@ -0,0 +1,11 @@
+public class stringscompare {
+    public static void main(String[] args) {
+        String name1="tony";
+        String name2="stark";
+        if(name1.compareTo(name2)==0){
+            System.out.println("They are equal");
+        }else{
+            System.out.println("Not equal");
+        }
+        }
+    }
